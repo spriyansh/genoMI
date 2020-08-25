a9120126f7cdcd4e253ba1e548a527c253085e85
vega <34842808+spriyansh@users.noreply.github.com>
2020-08-25 19:33:48 +0530

    Update

---
+++ BinaryClassifiers/estimate.py
@@ -0,0 +1,100 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.datasets import load_digits
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+
+from sklearn.metrics import confusion_matrix
+
+
+# function of statiscal variable calculations
+def estimate(y_test, y_pred):
+    cm = confusion_matrix(y_test, y_pred)
+
+    plt.figure(figsize = (2,2))
+    sns.heatmap(cm, annot=True, annot_kws={"size": 16})
+
+    # calculation of accuracy and related values
+    tp, fp, fn, tn = cm.ravel()
+
+    # Accuracy
+    print("Accuracy: "+ str(((tp + tn)/(tp + tn + fn + fp)) * 100))
+
+    # Specificity
+    print("Specificity: "+ str(((tn)/(tn + fp)) * 100))
+
+    # Senstivity
+    print("Senstivity: " + str(((tp)/(tp + fn)) * 100))
+    
+    # Precesion
+    pre = tp/(tp + fp)
+    print("Precision: " + str(pre))
+    
+    # Recall
+    rec = tp/(tp + fn)
+    print("Recall: " + str(rec))
+    
+    # F1 Score
+    print("F1 Score: " + str(2*((pre * rec)/(pre + rec))))
+
+# Plot function
+def plot_learning_curve(estimator, title, X, y, axes=None, ylim=None, cv=None,
+                        n_jobs=None, train_sizes=np.linspace(.1, 1.0, 5)):
+
+    if axes is None:
+        _, axes = plt.subplots(1, 3, figsize=(20, 5))
+
+    axes[0].set_title(title)
+    if ylim is not None:
+        axes[0].set_ylim(*ylim)
+    axes[0].set_xlabel("Training examples")
+    axes[0].set_ylabel("Score")
+
+    train_sizes, train_scores, test_scores, fit_times, _ = \
+        learning_curve(estimator, X, y, cv=cv, n_jobs=n_jobs,
+                       train_sizes=train_sizes,
+                       return_times=True)
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    fit_times_mean = np.mean(fit_times, axis=1)
+    fit_times_std = np.std(fit_times, axis=1)
+
+    # Plot learning curve
+    axes[0].grid()
+    axes[0].fill_between(train_sizes, train_scores_mean - train_scores_std,
+                         train_scores_mean + train_scores_std, alpha=0.1,
+                         color="r")
+    axes[0].fill_between(train_sizes, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1,
+                         color="g")
+    axes[0].plot(train_sizes, train_scores_mean, 'o-', color="r",
+                 label="Training score")
+    axes[0].plot(train_sizes, test_scores_mean, 'o-', color="g",
+                 label="Cross-validation score")
+    axes[0].legend(loc="best")
+
+    # Plot n_samples vs fit_times
+    axes[1].grid()
+    axes[1].plot(train_sizes, fit_times_mean, 'o-')
+    axes[1].fill_between(train_sizes, fit_times_mean - fit_times_std,
+                         fit_times_mean + fit_times_std, alpha=0.1)
+    axes[1].set_xlabel("Training examples")
+    axes[1].set_ylabel("fit_times")
+    axes[1].set_title("Scalability of the model")
+
+    # Plot fit_time vs score
+    axes[2].grid()
+    axes[2].plot(fit_times_mean, test_scores_mean, 'o-')
+    axes[2].fill_between(fit_times_mean, test_scores_mean - test_scores_std,
+                         test_scores_mean + test_scores_std, alpha=0.1)
+    axes[2].set_xlabel("fit_times")
+    axes[2].set_ylabel("Score")
+    axes[2].set_title("Performance of the model")
+
+    return plt
